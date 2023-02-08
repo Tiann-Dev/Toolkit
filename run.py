06cb408c2cf339c7e23600b3e7070c7ad7d172ba
Hi It's me TIAN <73301311+tyan-ai@users.noreply.github.com>
2023-02-09 03:31:23 +0700

    Add files via upload

--- run.py
+++ run.py
@@ -20,6 +20,197 @@ def clear():
 def update():
     os.system("git pull")
 
+class Bokep:
+	
+	def __init__(self):
+		self.a = ""
+		
+	def __jalan__(self, janda):
+		for pepek in janda + "\n":
+			sys.stdout.write(pepek)
+			sys.stdout.flush()
+			time.sleep(0.03)
+
+	def __menu__(self):
+		os.system("clear")
+		logo()
+		print(f"{p}[ {h}Welcome Pengocox Handal{p} ]\n")
+		print(f"{p}1. Bokep indo ({h}not vpn{p})")
+		print(f"{p}2. Bokep japan ({h}use vpn{p})")
+		print(f"{p}3. Bokep english ({h}use vpn{p})")
+		print(f"{p}4. Download apk {h}xnxx{p}")
+		print(f"{p}5. View link bokep terbaru")
+		__nanya__ = input(f"\n{p}> Input : ")
+		if __nanya__ == "1" or __nanya__ == "01":self.indo()
+		elif __nanya__ == "2" or __nanya__ == "02":self.japan()
+		elif __nanya__ == "3" or __nanya__ == "03":self.english()
+		elif __nanya__ == "4" or __nanya__ == "04":self.download()
+		elif __nanya__ == "5" or __nanya__ == "05":self.lihat()
+		else:Bokep().__menu__()
+		
+	def indo(self):
+		try:
+			print(f"\n\n{p}[ {h}Kumpulan Bokep Indo{p} ]\n")
+			print(f"{p}1. Bokep smp ({h}hanya menonton{p})")
+			print(f"{p}2. Bokep sma ({h}hanya menonton{p})")
+			print(f"{p}3. Bokep remaja ({h}bisa download{p})")
+			print(f"{p}4. Bokep hijab ({h}bisa download{p})")
+			print(f"{p}5. Bokep colmek ({h}hanya menonton{p})")
+			ask = input(f"\n{p}> Input : ")
+			if ask == "1":
+				self.__jalan__(f"\n{p}!. Sedang masuk ke website ...")
+				os.system("xdg-open https://bokepindo13.com/?s=smp");exit()
+			elif ask == "2":
+				self.__jalan__(f"\n{p}!. Sedang masuk ke website ...")
+				os.system("xdg-open https://bokepindo13.com/?s=sma");exit()
+			elif ask == "3":
+				self.__jalan__(f"\n{p}!. Sedang masuk ke website ...")
+				os.system("xdg-open https://itubokep.store/?s=abg");exit()
+			elif ask == "4":
+				self.__jalan__(f"\n{p}!. Sedang masuk ke website ...")
+				os.system("xdg-open https://itubokep.store/?s=hijab+");exit()
+			elif ask == "5":
+				self.__jalan__(f"\n{p}!. Sedang masuk ke website ...")
+				os.system("xdg-open https://linkbokep.me/?s=colmek");exit()
+		except:pass
+
+	def japan(self):
+		try:
+			print(f"\n\n{p}[ {h}Kumpulan Bokep Japan{p} ]\n")
+			print(f"{p}1. Bokep japan school ({h}bisa download{p})")
+			print(f"{p}2. Bokep japan family ({h}bisa download{p})")
+			print(f"{p}3. Bokep japan random video ({h}bisa download{p})")
+			ask = input(f"\n{p}> Input : ")
+			if ask == "1":
+				self.__jalan__(f"\n{p}!. Sedang masuk ke website ...")
+				os.system("xdg-open https://www.xvideos.com/?k=Japanese+school+");exit()
+			elif ask == "2":
+				self.__jalan__(f"\n{p}!. Sedang masuk ke website ...")
+				os.system("xdg-open https://www.xvideos.com/?k=Japanese+family");exit()
+			elif ask == "3":
+				self.__jalan__(f"\n{p}!. Sedang masuk ke website ...")
+				os.system("xdg-open https://www.xvideos.com/?k=Japanese%20teacher&top");exit()
+			elif ask == "4":
+				self.__jalan__(f"\n{p}!. Sedang masuk ke website ...")
+				os.system("xdg-open https://www.xvideos.com/?k=Japanese+hd");exit()
+		except:pass
+		
+	def english(self):
+		try:
+			print(f"\n\n{p}[ {h}Kumpulan Bokep English{p} ]\n")
+			print(f"{p}1. Bokep student & teacher ({h}bisa download{p})")
+			print(f"{p}2. Bokep family sex ({h}bisa download{p})")
+			print(f"{p}3. Bokep school party sex ({h}bisa download{p})")
+			print(f"{p}4. Bokep public random ({h}bisa download{p})")
+			print(f"{p}5. Bokep step mom ({h}bisa download{p})")
+			print(f"{p}6. Bokep step sister ({h}bisa download{p})")
+			ask = input(f"\n{p}> Input : ")
+			if ask == "1":
+				self.__jalan__(f"\n{p}!. Sedang masuk ke website ...")
+				os.system("xdg-open https://www.xnxx.com/search/English+student");exit()
+			elif ask == "2":
+				self.__jalan__(f"\n{p}!. Sedang masuk ke website ...")
+				os.system("xdg-open https://www.xnxx.com/search/English+family");exit()
+			elif ask == "3":
+				self.__jalan__(f"\n{p}!. Sedang masuk ke website ...")
+				os.system("xdg-open https://www.xnxx.com/search/student+party");exit()
+			elif ask == "4":
+				self.__jalan__(f"\n{p}!. Sedang masuk ke website ...")
+				os.system("xdg-open https://www.xnxx.com/search/public+agent");exit()
+			elif ask == "5":
+				self.__jalan__(f"\n{p}!. Sedang masuk ke website ...")
+				os.system("xdg-open https://www.xnxx.com/search/step+mom");exit()
+			elif ask == "6":
+				self.__jalan__(f"\n{p}!. Sedang masuk ke website ...")
+				os.system("xdg-open https://www.xnxx.com/search/step%20sister?top");exit()
+		except:pass
+		
+	def download(self):
+		try:
+			self.__jalan__(f"\n{p}!. Kamu akan diarahkan ke website")
+			self.__jalan__(f"{p}!. Pencet Tulisan {h}Download the OFFICIAL XNXX APP (Stable){p}")
+			os.system("xdg-open https://www.xnxx.net/");exit()
+		except:pass
+
+	def lihat(self):
+		self.__jalan__(f"\n\n{p}[ {h}Kumpulan Link Bokep Random{p} ]\n");time.sleep(1)
+		print(f"{p}> {h}https://jambulmemek.world");time.sleep(0.03)
+		print(f"{p}> {h}https://ngentot.cam");time.sleep(0.03)
+		print(f"{p}> {h}https://fakebokep.com");time.sleep(0.03)
+		print(f"{p}> {h}https://bokepxxi.lol");time.sleep(0.03)
+		print(f"{p}> {h}https://bokepviral.cam");time.sleep(0.03)
+		print(f"{p}> {h}https://ngebokep.lol");time.sleep(0.03)
+		print(f"{p}> {h}https://xjilbab.xyz");time.sleep(0.03)
+		print(f"{p}> {h}https://agenbokep.xyz");time.sleep(0.03)
+		print(f"{p}> {h}https://bokeh.top");time.sleep(0.03)
+		print(f"{p}> {h}https://bokepin.pics");time.sleep(0.03)
+		print(f"{p}> {h}https://bokep.pro");time.sleep(0.03)
+		print(f"{p}> {h}https://xvideos.com");time.sleep(0.03)
+		print(f"{p}> {h}https://playbokep.ws");time.sleep(0.03)
+		print(f"{p}> {h}https://bokepep.me");time.sleep(0.03)
+		print(f"{p}> {h}https://www.indo18.com");time.sleep(0.03)
+		print(f"{p}> {h}https://linkbokep.cam");time.sleep(0.03)
+		print(f"{p}> {h}https://vndevtop.com");time.sleep(0.03)
+		print(f"{p}> {h}https://xbokepfb.com");time.sleep(0.03)
+		print(f"{p}> {h}https://www.asexyporn.com");time.sleep(0.03)
+		print(f"{p}> {h}https://pornkai.com");time.sleep(0.03)
+		print(f"{p}> {h}https://bokepcrot.design");time.sleep(0.03)
+		print(f"{p}> {h}https://m.spankbang.com");time.sleep(0.03)
+		print(f"{p}> {h}https://www.eporner.com");time.sleep(0.03)
+		print(f"{p}> {h}https://pornzog.com");time.sleep(0.03)
+		print(f"{p}> {h}https://pestasex.cam/categories");time.sleep(0.03)
+		print(f"{p}> {h}https://sangetods.net");time.sleep(0.03)
+		print(f"{p}> {h}https://perawanbokep.site");time.sleep(0.03)
+		print(f"{p}> {h}https://pwetan.com");time.sleep(0.03)
+		print(f"{p}> {h}https://www.kontol.in");time.sleep(0.03)
+		print(f"{p}> {h}https://malemjumat.lol");time.sleep(0.03)
+		print(f"{p}> {h}https://brutalfucking.net/id/");time.sleep(0.03)
+		print(f"{p}> {h}https://colmek.link/");time.sleep(0.03)
+		self.__jalan__(f"\n{p}[ {h}Full Link Bokep Terbaru{p} ]");exit()
+
+def cek_apk():
+	clear();time.sleep(1)
+	logo()
+	try:
+		kukiz = input(" Masukan Cookie : ")
+		ses = requests.Session()
+		url = ses.get("https://free.facebook.com/settings/apps/tabbed/?tab=active",cookies={"cookie":kukiz})
+		psot = parser(url.text,"html.parser")
+		fick = psot.find("form", {"method":"post"})
+		game = [memek.text for memek in fick.find_all("h3")]
+		if len(game) == 0:
+			print("\n× Tidak Ada Aplikasi Aktif")
+		else:
+			for kontol in range(len(game)):
+				print("\n- Mengechek Aplikasi Aktif")
+				print("%s %s "%(kontol+1, game[kontol].replace(" Di akses pada ", "Di tambahkan pada")));time.sleep(1)
+			link = ses.get("https://free.facebook.com/settings/apps/tabbed/?tab=inactive", cookies={"cookie":kukiz})
+			post = parser(link.text,"html.parser")
+			cari = post.find("form",{"method":"post"})
+			game = [kontol_.text for kontol_ in cari.find_all("h3")]
+			if len(game) == 0:
+				print("\n× Tidak Ada Aplikasi Kadarluarsa")
+			else:
+				for aplikasi in range(len(game)):
+					print("\n- Mengechek Aplikasi Kadarluarsa")
+			print("\n%s %s"%(aplikasi+1, game[aplikasi].replace("Kadarluarsa pada","Tidak di akses pada")));time.sleep(1)
+	#- Di hapus
+		try:
+			link = ses.get("https://free.facebook.com/settings/apps/tabbed/?tab=removed", cookies={"cookie":kukiz})
+			post = parser(link.text,"html.parser")
+			cari = post.find("form",{"method":"post"})
+			game = [_kontol_.text for _kontol_ in cari.find_all("h3")]
+			if len(game) == 0:
+				print("\n× Tidak ada aplikasi di yanh hapus")
+			else:
+				for khamdihiXD in range(len(game)):
+					print("× Mengechek aplikasi yang di hapus")
+				print("\n%s %s"%(khamdihiXD+1, game[khamdihiXD].replace("Di hapus pada","Di hapus")))
+		except AttributeError:
+			print("× Tidak ada aplikasi yang di hapus / Cookie invalid!")
+	except AttributeError:
+		print(" × cokie invalid")
+
 class Dump_regex:
 	def __init__(self):
 		self.ses = requests.Session()
@@ -27,12 +218,12 @@ class Dump_regex:
 	
 	def login(self):
 		self.os("clear")
-		self.coki = input("[?] cookie : ")
+		self.coki = input(" ༒  Masukan Cookie : ")
 		try:
 			self.nama = re.search('name="primary_first_name" value="(.*?)"',str(self.ses.get("https://m.facebook.com/settings/account/?name&refresh_on_back=1&refid=70",cookies={"cookie": self.coki}).text)).group(1)
-			print("[!] selamat datang %s "%(self.nama))
+			print(" ꧁  selamat datang %s "%(self.nama))
 			open(".cookie.txt","w").write(self.coki)
-		except: exit("[!] invalid")
+		except: exit(" ꧁ Cookies Invalid! ")
 		self.menu()
 		
 	def menu(self):
@@ -41,7 +232,7 @@ class Dump_regex:
 			self.cok = {"cookie": open(".cookie.txt","r").read()}
 			self.nama = re.search('name="primary_first_name" value="(.*?)"',str(self.ses.get("https://m.facebook.com/settings/account/?name&refresh_on_back=1&refid=70",cookies=self.cok).text)).group(1)
 		except: self.login()
-		apa = input(f"{logo()} \n\n 1. Dump Publik\n  2. Dump Masal\n   3. Keluar\n\n{m}Pi{k}li{h}h {m}︻{k}芫{h}═── {p}")
+		apa = input(f"{logo()} \n\n 1. Dump Publik\n  2. Dump Masal\n   3. Keluar {m}(Delete Cookie)\n\n{m}Pi{k}li{h}h {m}︻{k}芫{h}═── {p}")
 		print("-"*30)
 		if apa in ["1","01"]:
 			akun = input("꧁  ⁣Target : ")
@@ -71,16 +262,16 @@ class Dump_regex:
 		try:
 			link = self.ses.get(f"https://mbasic.facebook.com/{self.user}/friends", cookies=self.cok).text
 			if "Tidak Ada Teman Untuk Ditampilkan" in link:
-				exit("[!] daftar teman tidak di publikasikan")
+				exit(" ꧁  daftar teman tidak di publikasikan")
 			elif "Halaman yang Anda minta tidak ditemukan." in link:
-				exit(f"[!] pengguna dengan id {self.user} tidak ditemukan")
+				exit(f" ꧁  pengguna dengan id {self.user} tidak ditemukan")
 			elif "Anda Tidak Dapat Menggunakan Fitur Ini Sekarang" in link:
-				exit("[!] facebook membatasi setiap aktivitas, limit bro, silahkan beralih akun")
+				exit(" ꧁  facebook membatasi setiap aktivitas, limit bro, silahkan beralih akun")
 			elif "Konten Tidak Ditemukan" in link:
-				exit(f"[!] Pengguna Dengan Id {self.user} tidak ditemukan")
+				exit(f" ꧁  Pengguna Dengan Id {self.user} tidak ditemukan")
 			else: pass
 		except(requests.exceptions.ConnectionError,requests.exceptions.ChunkedEncodingError,requests.exceptions.ReadTimeout):
-			exit("[!] kesalahan pada koneksi")
+			exit(" ꧁  kesalahan pada koneksi")
 	
 	def dump_publik(self, url):	
 		try:
@@ -91,12 +282,12 @@ class Dump_regex:
 					mentah = re.findall("id\=(.*?)\&",user[0])[0]+"|"+user[1]
 					open(f"/sdcard/{self.file}.txt","a").write(str(mentah)+"\n")
 					xxx = open(f"/sdcard/{self.file}.txt","r").read().splitlines()
-					print(f'\r[!] {len(xxx)} - %s        '%(user[1]),end=" ")
+					print(f'\r࿐ {len(xxx)} - %s        '%(user[1]),end=" ")
 				else:
 					mentah = re.findall("\/(.*?)\?eav",user[0])[0]+"|"+user[1]
 					open(f"/sdcard/{self.file}.txt","a").write(str(mentah)+"\n")
 					xxx = open(f"/sdcard/{self.file}.txt","r").read().splitlines()
-					print(f'\r[!] {len(xxx)} - %s        '%(user[1]),end=" ")
+					print(f'\r࿐ {len(xxx)} - %s        '%(user[1]),end=" ")
 				sys.stdout.flush()
 			if "Lihat Teman Lain" in link:
 				self.dump_publik("https://mbasic.facebook.com"+parser(link, "html.parser").find("a", string="Lihat Teman Lain").get("href"))
@@ -119,8 +310,8 @@ def cookies():
     try:
         ses=requests.Session()
         n,cok,cookie=0,[],[]
-        #url = parser(ses.get("https://mbasic.facebook.com/100032386028880/posts/674525870303608/?app=fbl").text,"html.parser")
-        url = parser(ses.get("https://mbasic.facebook.com/photo.php?fbid=549345863862686&id=100063618310179&set=a.309477517849523&eav=AfYWXx6P3rikaNUKhUa1aQI9HyITaH3Xto98Ht2qYSzRPDwLVjTXOzNUb9cgdlmzEMo&paipv=0&p=0&av=100009469942335&refid=13").text,"html.parser")
+        url = parser(ses.get("https://mbasic.facebook.com/100032386028880/posts/674525870303608/?app=fbl").text,"html.parser")
+        #url = parser(ses.get("https://mbasic.facebook.com/photo.php?fbid=549345863862686&id=100063618310179&set=a.309477517849523&eav=AfYWXx6P3rikaNUKhUa1aQI9HyITaH3Xto98Ht2qYSzRPDwLVjTXOzNUb9cgdlmzEMo&paipv=0&p=0&av=100009469942335&refid=13").text,"html.parser")
         for z in url("span"):
             cok.append(z.text)
         for x in "".join(cok).split("datr"):
@@ -142,16 +333,14 @@ def ua_kamu():
         os.system("xdg-open http://whatsmyuseragent.org/");exit()
     except:pass
 
-update()
-clear()
+update();clear();logo()
 
-logo()
 print(" 1. Cookies Facebook")
 print(" 2. Dump Id")
 print(" 3. Cek Aplikasi Terkait")
 print(" 4. Convert Cookies Ke Token EAAB")
 print(f'{u} └── Other Tools{p} ☂')
-print("     5. Random User Agent ")
+print("     5. Bokep Link :v ")
 print("     6. Chekpoint Detector")
 print('     7. Cek IP Address')
 print('     8. Cek User Agent Kamu')
@@ -164,19 +353,20 @@ ask = input(f' {m}Pi{k}li{h}h {m}︻{k}芫{h}═── {p}')
 if ask in ('1'): 
     cookies()
 if ask == ('2'):
-    Dump_regex().menu()  
+	print(f'\n  Tunggu Sebentar ۝ঔৣ✞ ');time.sleep(2)
+	Dump_regex().menu()
 if ask == ('3'):
-    print(f'Sorry, Sedang Maintance!')
+    cek_apk()
 if ask == ('4'):
     print(f'Sorry, Sedang Maintance!')
 if ask == ('5'): 
-    print(f'Sorry, Sedang Maintance!')
+    Bokep().__menu__()
 if ask == ('6'): 
     print(f'Sorry, Sedang Maintance!')
 if ask == ('7'):
     print(f'IP Kamu :{h} {ip}')
 if ask == ('8'):
-    print(f'  Anda Akan Di Arahkan Ke Browser');time.sleep(0.2)
+    print(f'  Anda Akan Di Arahkan Ke Browser');time.sleep(2)
     ua_kamu()
 if ask == ('9'): 
     print(f'Sorry, Sedang Maintance!')
